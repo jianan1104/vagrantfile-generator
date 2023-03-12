32129e563d127df1358c2b76483d0271b23142d5
Kane <kane@KanedeMacBook-Pro.local>
2023-03-13 04:09:37 +0800

    feat: set JIT mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 module.exports = {
+  mode: 'jit',
   content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
+  ],
+  purge: [
+    "./public/**/*.html",
     "./src/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
     "./Components/**/*.{js,ts,jsx,tsx}",
