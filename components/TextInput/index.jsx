8621be7e92e5e64fbfa7ab6451b69707e4b40bf3
Amin Saedi <amin.di.sam@gmail.com>
2023-10-21 19:42:25 -0400

    added feature to create multiple machines

--- components/TextInput/index.jsx
+++ components/TextInput/index.jsx
@@ -1,4 +1,4 @@
-const TextInput = ({ name, value, holder, handleInputChange, formData, children}) => {
+const TextInput = ({ name, value, holder, type, handleInputChange, formData, children}) => {
   return (
     <div className="mb-4">
       <label
@@ -10,7 +10,7 @@ const TextInput = ({ name, value, holder, handleInputChange, formData, children}
       </label>
       <input
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        type="text"
+        type={type || "text"}
         name={value}
         id={value}
         value={formData}
