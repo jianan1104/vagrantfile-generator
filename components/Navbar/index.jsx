b9f3b60a3974fa4e46a59527357cc1747a2aec25
Jian An, Lin <76777822+jianan1104@users.noreply.github.com>
2024-04-22 15:29:32 +0800

    Fix url typo

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
-        <a href="https://flowbite.com/" className="flex items-center">
+        <a href="https://vagrantfile-generator.vercel.app/" className="flex items-center">
           <img src="images/vagrant-icon.png" className="h-6 mr-3 sm:h-10" alt="Flowbite Logo"/>
           <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
             VagrantFile Generator
