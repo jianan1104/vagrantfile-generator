31fffa4f052416dd7ef0068c3f3a6053600955dd
Kane <kane@KanedeMBP.localdomain>
2026-02-01 00:14:39 +0800

    chore: sync updated box list with correct Rocky Linux SVG icon to main

--- components/constants/boxes.js
+++ components/constants/boxes.js
@@ -1,9 +1,15 @@
 const boxes = [
   {
-    id: "ubuntu2004",
-    value: "ubuntu/focal64",
-    name: "Ubuntu 20.04",
+    id: "ubuntu2204",
+    value: "ubuntu/jammy64",
+    name: "Ubuntu 22.04",
     src: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Logo-ubuntu_cof-orange-hex.svg",
+  },
+  {
+    id: "rockylinux9",
+    value: "rockylinux/9",
+    name: "Rocky Linux 9",
+    src: "https://upload.wikimedia.org/wikipedia/commons/7/77/Rocky_Linux_logo.svg",
   },
   {
     id: "centos7",
@@ -12,15 +18,15 @@ const boxes = [
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Centos-logo-light.svg/1024px-Centos-logo-light.svg.png",
   },
   {
-    id: "debian10",
-    value: "debian/buster64",
-    name: "Debian 10",
+    id: "debian12",
+    value: "debian/bookworm64",
+    name: "Debian 12",
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Debian-OpenLogo.svg/1024px-Debian-OpenLogo.svg.png",
   },
   {
-    id: "fedora34",
-    value: "generic/fedora34",
-    name: "Fedora 34",
+    id: "fedora40",
+    value: "fedora/40-cloud-base",
+    name: "Fedora 40",
     src: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Fedora_icon_%282021%29.svg/2089px-Fedora_icon_%282021%29.svg.png",
   },
 ];
